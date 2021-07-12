d7c9d4bafc39c25ef5c6a73201e3abddf3a3b1d3
Pablo Guerra <pguerrabedoya@gmail.com>
2021-07-12 09:31:24 -0500

    Hoja de vida principal

---
+++ js.js
@@ -0,0 +1,19 @@
+// const button = "<button>Botón</button>"
+// // const btt = document.getElementById("button")
+// const column = document.getElementsByClassName("card");
+// column[0].classList.add("bg-red")
+// for (let i = 0; i < column.length; i++) {
+//     column[i].classList.add("bg-red")
+// }
+
+// column[0].innerHTML += button;
+
+
+
+
+
+
+
+
+
+
